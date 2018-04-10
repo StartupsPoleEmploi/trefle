e03084aa9035e26d6265aa74afdd83eef5e5a7c7
Yohan Boniface <yb@enix.org>
2018-04-10 09:00:21 +0200

    Only check passed rules in usecases

--- usecases/test_cases.py
+++ usecases/test_cases.py
@@ -7,16 +7,15 @@ from pff.core import simulate
 
 # Magic: Generate tests from yml.
 def pytest_generate_tests(metafunc):
-    if {'input', 'passed', 'failed'} <= set(metafunc.fixturenames):
+    if {'input', 'expected'} <= set(metafunc.fixturenames):
         args = []
         ids = []
         with (Path(__file__).parent / 'cases.yml').open() as fixtures:
             for fixture in yaml.load(fixtures):
-                args.append((fixture['entrée'], fixture['sortie']['éligible'],
-                             fixture['sortie']['non éligible']))
-                ids.append(str(fixture['entrée']))
-            metafunc.parametrize('input,passed,failed', args, ids=ids)
+                args.append((fixture['entrée'], fixture['sortie']))
+                ids.append(fixture.get('description', str(fixture['entrée'])))
+            metafunc.parametrize('input,expected', args, ids=ids)
 
 
-def test_abrev(input, passed, failed):
-    assert list(simulate(**input)) == [passed or [], failed or []]
+def test_abrev(input, expected):
+    assert list(simulate(**input))[0] == (expected or [])
