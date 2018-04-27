cc9b13ea1cea30dba77961a768932227261aad16
Yohan Boniface <yb@enix.org>
2018-04-27 17:48:33 +0200

    Remove bad copy-paste

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ VERSION = (0, 1, 0)
 
 __author__ = 'Yohan Boniface'
 __contact__ = "yohan.boniface@data.gouv.fr"
-__homepage__ = "https://github.com/pyrates/roll"
+__homepage__ = "https://framagit.org/ybon/trefle"
 __version__ = ".".join(map(str, VERSION))
 
 setup(
