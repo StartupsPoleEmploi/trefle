4fe36744979f00723aa28cedaefab9d93bba81a7
Yohan Boniface <yb@enix.org>
2018-06-21 11:24:21 +0200

    0.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def is_pkg(line):
 with open('requirements.txt', encoding='utf-8') as reqs:
     install_requires = [l for l in reqs.read().split('\n') if is_pkg(l)]
 
-VERSION = (0, 1, 3)
+VERSION = (0, 2, 0)
 
 __author__ = 'Yohan Boniface'
 __contact__ = "yohan.boniface@data.gouv.fr"
