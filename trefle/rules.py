3ac985cd9066dd5850bac75e11cb6b9e44d46181
Yohan Boniface <yb@enix.org>
2018-07-10 11:01:15 +0200

    Split Action and Condition
    Logic will diverge

--- trefle/rules.py
+++ trefle/rules.py
@@ -130,10 +130,20 @@ class Status:
         return out
 
 
-class Step:
+class Action:
+
+    PATTERNS = {}
+
+    def __init__(self, raw, name=None, line=0):
+        self.raw = raw
+        self.func = None
+        self.params = {}
+        self.name = name
+        self.line = line
+        self.compile()
 
     def __repr__(self):
-        return f'<{self.__class__.__name__}: {self}>'
+        return f'<Action: {self}>'
 
     def __str__(self):
         return self.raw
@@ -145,7 +155,7 @@ class Step:
                 self.func = func
                 break
         else:
-            raise NoStepError(f'No pattern match step: `{self.raw}`')
+            raise NoStepError(f'No pattern match action: `{self.raw}`')
         data = match.groupdict()
         spec = inspect.signature(self.func)
         for name, param in spec.parameters.items():
@@ -161,19 +171,6 @@ class Step:
                     raise
             self.params[name] = value
 
-
-class Action(Step):
-
-    PATTERNS = {}
-
-    def __init__(self, raw, name=None, line=0):
-        self.raw = raw
-        self.func = None
-        self.params = {}
-        self.name = name
-        self.line = line
-        self.compile()
-
     def act(self, context):
         try:
             self.func(context, **self.params)
@@ -185,7 +182,48 @@ class Action(Step):
             self.func.on_miss(status, context, **self.params)
 
 
-class Condition(Step):
+@action(r"(l'|les? |la )(?P<key>.+) (vaut|est) (?P<value>[\w«» +\-'\.]+)$")
+@action(r"(l'|les? |la )(?P<key>.+) est égale? (à la|à|aux?)? (?P<value>[\w«» +\-\.']+)$")
+def set_value(context, key: Label, value: LazyValue):
+    context[key] = value.get(**context)
+
+
+@action(r"(l'|les? |la )(?P<key>.+) est égale? à (?P<rate>[\d\.]+)% (de la|du) (?P<value>[\w«» +\-\.']+)$")
+def set_percent(context, key: Label, rate: float, value: LazyValue):
+    context[key] = value.get(**context) * rate / 100
+
+
+def attach_status(status, context, name):
+    conditions = [status.json]
+    while status.parent is not None:
+        status = status.parent
+        conditions.append(status.json)
+    conditions.reverse()
+    context['financements'][name]['eligibilite'] = conditions
+
+
+@on_miss(attach_status)
+@action(r"définir le financement «(?P<name>[\w +-]+)» comme éligible")
+def set_financement_eligible(context, name):
+    if name not in context['financements']:
+        raise ValueError(f'Unknown financement `{name}`')
+    context['financements'][name]['eligible'] = True
+
+
+@action(r"c'est une? (?P<key>.+)")
+def set_true(context, key: Label):
+    context[key] = True
+
+
+@action(r"il n'y a pas de (?P<key>[\w ]+)$")
+def unset_key(context, key: Label):
+    try:
+        del context[key]
+    except KeyError:
+        pass
+
+
+class Condition:
 
     PATTERNS = {}
     AND = 'ET'
@@ -219,6 +257,35 @@ class Condition(Step):
         else:
             self.compile()
 
+    def __repr__(self):
+        return f'<Condition: {self}>'
+
+    def __str__(self):
+        return self.raw
+
+    def compile(self):
+        for pattern, func in self.PATTERNS.items():
+            match = pattern.match(self.raw)
+            if match:
+                self.func = func
+                break
+        else:
+            raise NoStepError(f'No pattern match step: `{self.raw}`')
+        data = match.groupdict()
+        spec = inspect.signature(self.func)
+        for name in list(spec.parameters.keys())[1:]:  # Skip context.
+            value = data[name]
+            try:
+                value = LazyValue(value)
+            except Exception as err:
+                # Give more context.
+                err.args = (f'{err} (from `{self.raw}`)',)
+                raise
+            self.params[name] = value
+
+    def get_params(self, context):
+        return {n: v.get(**context) for n, v in self.params.items()}
+
     def assess(self, context, parent=None, overall=True):
         status = Status(self, parent)
         if self.terms:
@@ -229,7 +296,7 @@ class Condition(Step):
                 current = all(status.terms)
         else:
             try:
-                current = self.func(context, **self.params)
+                current = self.func(context, **self.get_params(context))
             except NoDataError:
                 current = False
             except Exception as err:
@@ -254,107 +321,67 @@ class Condition(Step):
         return status
 
 
-def attach_status(status, context, name):
-    conditions = [status.json]
-    while status.parent is not None:
-        status = status.parent
-        conditions.append(status.json)
-    conditions.reverse()
-    context['financements'][name]['eligibilite'] = conditions
-
-
-@action(r"(l'|les? |la )(?P<key>.+) (vaut|est) (?P<value>[\w«» +\-'\.]+)$")
-@action(r"(l'|les? |la )(?P<key>.+) est égale? (à la|à|aux?)? (?P<value>[\w«» +\-\.']+)$")
-def set_value(context, key: Label, value: LazyValue):
-    context[key] = value.get(**context)
-
-
-@action(r"(l'|les? |la )(?P<key>.+) est égale? à (?P<rate>[\d\.]+)% (de la|du) (?P<value>[\w«» +\-\.']+)$")
-def set_percent(context, key: Label, rate: float, value: LazyValue):
-    context[key] = value.get(**context) * rate / 100
-
-
-@on_miss(attach_status)
-@action(r"définir le financement «(?P<name>[\w +-]+)» comme éligible")
-def set_financement_eligible(context, name):
-    if name not in context['financements']:
-        raise ValueError(f'Unknown financement `{name}`')
-    context['financements'][name]['eligible'] = True
-
-
-@action(r"c'est une? (?P<key>.+)")
-def set_true(context, key: Label):
-    context[key] = True
-
-
-@action(r"il n'y a pas de (?P<key>[\w ]+)$")
-def unset_key(context, key: Label):
-    try:
-        del context[key]
-    except KeyError:
-        pass
-
-
 @condition(r"c'est une? (?P<key>.+)")
-def check_true(context, key: LazyValue):
-    return key.get(**context) is True
+def check_true(context, key):
+    return key is True
 
 
 @condition(r"ce n'est pas une? (?P<key>.+)")
-def check_false(context, key: LazyValue):
-    return key.get(**context) is False
+def check_false(context, key):
+    return key is False
 
 
 @condition(r"le financement est de type (?P<tag>.+)")
-def check_type(context, tag: LazyValue):
-    return tag.get(**context) in context['financement.tags']
+def check_type(context, tag):
+    return tag in context['financement.tags']
 
 
 @condition(r"(l'|les? |la )(?P<left>.+) est supérieure? à (?P<right>[\w ]+)")
-def check_gt(context, left: LazyValue, right: LazyValue):
-    return left.get(**context) > right.get(**context)
+def check_gt(context, left, right):
+    return left > right
 
 
 @condition(r"(l'|les? |la )(?P<left>.+) est supérieure? ou égale? à (?P<right>[\w ]+)")
-def check_ge(context, left: LazyValue, right: LazyValue):
-    return left.get(**context) >= right.get(**context)
+def check_ge(context, left, right):
+    return left >= right
 
 
 @condition(r"(l'|les? |la )(?P<left>.+) est inférieure? à (?P<right>[\w ]+)")
-def check_lt(context, left: LazyValue, right: LazyValue):
-    return left.get(**context) < right.get(**context)
+def check_lt(context, left, right):
+    return left < right
 
 
 @condition(r"(l'|les? |la )(?P<left>.+) est inférieure? ou égale? à (?P<right>[\w ]+)")
-def check_le(context, left: LazyValue, right: LazyValue):
-    return left.get(**context) <= right.get(**context)
+def check_le(context, left, right):
+    return left <= right
 
 
 @condition(r"(l'|les? |la )(?P<left>.+) contien(nen)?t au moins une? ([^ ]+ )?(parmi|des) (?P<right>[ \[\],\w«»]+)")
-def check_share_one(context, left: LazyValue, right: LazyValue):
-    return len(set(left.get(**context) or []) & set(right.get(**context) or [])) > 0
+def check_share_one(context, left, right):
+    return bool(set(left or []) & set(right or []))
 
 
+# @not_match('{left & right}')
 @condition(r"(l'|les? |la )(?P<left>.+) ne contien(nen)?t aucun des (?P<right>[ \w«»]+)")
-def check_not_share_one(context, left: LazyValue, right: LazyValue):
-    return len(set(left.get(**context) or []) & set(right.get(**context) or [])) == 0
+def check_not_share_one(context, left, right):
+    return not bool(set(left or []) & set(right or []))
 
 
 @condition(r"(l'|les? |la )(?P<left>.+) fait partie (de l'|de la |des? |du )(?P<right>.+)")
 @condition(r"(l'|les? |la )(?P<right>.+) contient (l'|les? |la )?(?P<left>[ \w«»]+)")
-def check_contain(context, left: LazyValue, right: LazyValue):
-    return left.get(**context) in right.get(**context)
+def check_contain(context, left, right):
+    return left in right
 
 
 @condition(r"(l'|les? |la )(?P<left>.+) ne fait pas partie (de l'|des? |de la |du )(?P<right>.+)")
 @condition(r"(l'|les? |la )(?P<right>.+) ne contient pas (l'|les? |la )?(?P<left>[ \w«»]+)")
-def check_not_contain(context, left: LazyValue, right: LazyValue):
-    return left.get(**context) not in right.get(**context)
+def check_not_contain(context, left, right):
+    return left not in right
 
 
 @condition(r"(l'|les? |la )(?P<left>.+) (est|vaut) (?P<right>[\w«» +\-\.']+)")
-def check_equal(context, left: LazyValue, right: LazyValue):
-    return left.get(**context) == right.get(**context)
+def check_equal(context, left, right):
+    return left == right
 
 
 Line = namedtuple('Line', ['index', 'indent', 'keyword', 'sentence'])
