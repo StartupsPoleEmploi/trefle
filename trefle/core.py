083e6c462457026f1dffc837ecc71d9fd8ff21cc
Yohan Boniface <yb@enix.org>
2018-08-08 09:43:29 +0200

    Hackish way to expose back a clean context
    When we call `simulate` we pass the data context per reference,
    and we use this context back for debugging and showing in the
    explorer for example.
    
    Until the recent "validation refactor", this context was updated
    with cleaned data, but this was no more the case, because the
    validation occurs while running the rules (instead of once and for
    all at the start).

--- trefle/core.py
+++ trefle/core.py
@@ -7,7 +7,7 @@ from .helpers import flatten
 async def simulate(data):
     # Prepare context
     flatten(data)
-    context = Context(data)
+    context = Context(data.copy())
     routine.extrapolate_context(context)
     await routine.populate_formation(context)
     routine.preprocess(context)
@@ -15,6 +15,15 @@ async def simulate(data):
     financements = [f.copy() for f in FINANCEMENTS]
     # Compute organisme, prise en charge, rémunération per financement
     for financement in financements:
-        routine.check_financement(context.copy(), financement)
+        copy = context.copy()
+        routine.check_financement(copy, financement)
+        # Do our best to show cleaned data to user eyes.
+        data.update(copy.cleaned_data)
+
+    # FIXME (limits of the single-store-all context object)
+    # Clean keys not meant to be exposed (some should not be in context)
+    for key in list(data.keys()):
+        if key.startswith(('status', 'parent', 'financement')):
+            del data[key]
 
     return financements
