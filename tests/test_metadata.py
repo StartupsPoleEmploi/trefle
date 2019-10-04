bc038e9bcf561eee92a9b85306fca9fff49f3732
David Foucher <david.foucher@pole-emploi.fr>
2019-10-04 18:53:29 +0200

    WIP add moficiation date
    TODO: call API endpoint for a rule with last modification date added or
    juste modification date returned as other metadata info

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -9,4 +9,4 @@ pytestmark = pytest.mark.asyncio
 
 async def test_last_modified():
     date = await last_modified('trefle/__init__.py')
-    assert date.isoformat() == '2018-12-07T11:18:40+01:00'
+    assert date == '2018-12-07'
