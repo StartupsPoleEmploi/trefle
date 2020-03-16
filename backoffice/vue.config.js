f3762a754f71c1cbe0f70f1f0802b594ec467808
Lucas Détré <lucas.detre@gmail.com>
2020-03-16 14:30:24 +0100

    changing server url in vue config

--- backoffice/vue.config.js
+++ backoffice/vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     publicPath: "/0.7/explorer/",
     assetsDir: "src/assets/",
     devServer: {
-        proxy: 'https://trefle.beta.pole-emploi.fr/0.6/'
+        proxy: 'https://trefle.beta.pole-emploi.fr/0.7/'
     }
 }
