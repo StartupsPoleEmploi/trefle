64f991822364eff2b12ab5b4a938a834c7b2aed0
Lucas Détré <lucas.detre@gmail.com>
2020-04-15 10:34:46 +0200

    fix issue #10 autofocus on rule content's textarea

--- backoffice/src/components/Rule.vue
+++ backoffice/src/components/Rule.vue
@@ -49,7 +49,7 @@
             </div>
             <div class="row mb-3">
               <label for="content"><u>Contenu de la règle</u></label>
-              <textarea-autosize id="content" v-model="content" class="rule-modification-text" :class="{editErrorClass: error_flags.notModified }"></textarea-autosize>
+              <textarea-autosize id="content" v-model="content" class="rule-modification-text" :class="{editErrorClass: error_flags.notModified }" autofocus></textarea-autosize>
               <span v-if="error_flags.notModified" class="text-danger font-weight-light">Aucune modification n'a été renseignée</span>
             </div>
             <div class="row mb-3">
