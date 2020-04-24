8354136609b77f009ed8f9746a189e96ec9b92d8
David Foucher <david.foucher@pole-emploi.fr>
2020-04-24 17:45:51 +0200

    Add default empty aythorisation file

--- docker/update_access.sh
+++ docker/update_access.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 docker cp authorisations.csv `docker ps -n 1 --filter "name=_trefle_" -q`:/srv/.
 docker container exec -it `docker ps -n 1 --filter "name=_trefle_" -q` /bin/bash /srv/start.sh
