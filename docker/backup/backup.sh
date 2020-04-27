354862486885b02c0b47aafda82046f4a0847d83
David Foucher <david.foucher@pole-emploi.fr>
2020-04-27 13:01:45 +0200

    Add exclude pattern

--- docker/backup/backup.sh
+++ docker/backup/backup.sh
@@ -2,10 +2,10 @@
 DATE=`date +%A`
 DAY=`date +%d`
 DIR="."
-TREFLE_DATA="trefle-data_${DAY}.tar"
+TREFLE_DATA="trefle-data_${DAY}.tar.gz"
 if [ "$1" ]
 then
         DIR="$1"
 fi
 
-tar --exclude=data/* --exclude=log/* -cjvf $DIR/$TREFLE_DATA /home/docker/*
+tar --exclude=data/* --exclude=log/* --exclude=*.tar.gz -czvf $DIR/$TREFLE_DATA /home/docker/*
